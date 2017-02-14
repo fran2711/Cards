2f744509bb38aa91d38467349fb31fbfeab23c89
Fran Lucena <Fran@Fran-MacBook-Pro.local>
2017-02-14 23:21:42 +0100

    Implemented DeckOfCardsAPIManager and Deck

--- Cards/DeckOfCardsAPIManager.swift
+++ Cards/DeckOfCardsAPIManager.swift
@@ -12,6 +12,30 @@ import Foundation
 public class DeckOfCardsAPIManager {
     
     
+    public func downloadDeck(completion: (Deck) -> Void) {
+        
+        let urlString = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+        
+        if let url = URL(string: urlString) {
+            do {
+                let data = Data(contentsOf: url)
+                let cardJson = JSONSerialization.jsonObject(with: data, options: .allowFragments) as! Dictionary<String, Any>
+                
+                
+            } catch {
+                
+            }
+        }
+        
+        // Lo paso a segundo plano
+        DispatchQueue.global().async { 
+            
+            
+            
+        }
+        
+        
+    }
     
     
 }
